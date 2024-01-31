89e7c8f905d2b8419c169a713345846c3b319186
Ariko Yahya Setyawan <152178505+ArikoYah@users.noreply.github.com>
2024-01-31 15:02:02 +0700

    Update cloudcomputing.py

--- cloudcomputing.py
+++ cloudcomputing.py
@@ -43,3 +43,15 @@ with col2:
   st.caption('Plot Cos')
   x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) #Generating x values from -2*pi to 2*pi
   y = np.cos(x) # Calculating sin(x) values
+  
+  fig, ax = plt.subplots(figsize=(16, 8))
+  ax.plot(x, y, label='cos(x)', color='g') # plotting sin(x) curve
+  ax.set_ylabel("Cos x")
+  ax.set_xlabel("x")
+  ax.tick_params(axis='y', labelsize=20)
+  ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right')
+  ax.tick_params(axis='x', labelsize=15)
+  
+  st.pyplot(fig)
+
+st.caption('Copyright @ Ariko Yahya 2024')
