fa8060935b485438eef213343802f8ffb54b743a
Ariko Yahya Setyawan <152178505+ArikoYah@users.noreply.github.com>
2024-01-31 14:56:15 +0700

    Update cloudcomputing.py

--- cloudcomputing.py
+++ cloudcomputing.py
@@ -20,3 +20,26 @@ ax.set_xticklabels(ax.get_xticklabels(),rotation=30, ha='right')
 ax.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig)
+
+# subheader
+st.subheader("Plot Sin & Cos")
+
+co11,co12 = st.coloumn(2)
+
+  with col1:
+  st.caption('Plot Sin')
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) # Generating x values from -2*pi to 2*pi
+  y = np.sin(x) # Calculating sin(x) values
+  fig, ax = plt.subplots(figsize=(16, 8))
+  ax.plot(x, y, label='sin(x)', color='b') # plotting sin(x) curve
+  ax.set_ylabel("Sin x")
+  ax.set_xlabel("x")
+  ax.tick_params(axis='y', labelsize=20)
+  ax.set_xticklabels(ax.get_sticklabels(), rotation=30, ha='right')
+  ax.tick_params(axis='x', labelsize=15)
+  st.pyplot(fig)
+
+with col2:
+  st.caption('Plot Cos')
+  x = np.linspace(-2 * np.pi, 2 * np.pi, 1000) #Generating x values from -2*pi to 2*pi
+  y = np.cos(x) # Calculating sin(x) values
