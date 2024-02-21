be0b5476acbae26b87c8837c15ca196bb8dba60e
Ariko Yahya Setyawan <152178505+ArikoYah@users.noreply.github.com>
2024-02-21 14:32:36 +0700

    Update sliderlaenz.py

--- sliderlaenz.py
+++ sliderlaenz.py
@@ -32,28 +32,33 @@ y_vals = f(x_vals)
 integral_value = trapezoidal_integral(f, x_range[0], x_range[1], 1000)
 
 # Plot the function and shaded region
-fig, ax = plt.subplots(2, 1, figsize=(16, 12), sharex=True)
+fig, ax = plt.subplots(2, 1, figsize=(16, 12))
 
-# Plot the function
+# Plot for the function
 ax[0].plot(x_vals, y_vals, label='f(x) = x^2 + 17x + 9', color='b')
 ax[0].fill_between(x_vals, y_vals, color='skyblue', alpha=0.3)  # Color the area under the curve
 ax[0].set_ylabel("f(x)")
-ax[0].tick_params(axis='y', labelsize=20)
+ax[0].set_xlabel("x")
+ax[0].tick_params(axis='y', labelsize=15)
+ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=30, ha='right')
+ax[0].tick_params(axis='x', labelsize=12)
 ax[0].grid(color='green', linestyle='-.', linewidth=.5)
 
-# Plot the integral approximation
-integral_approximation = np.linspace(x_range[0], x_range[1], 10)
-integral_approximation_y = f(integral_approximation)
-ax[1].plot(integral_approximation, integral_approximation_y, marker='o', linestyle='-', color='r')
-ax[1].fill_between(integral_approximation, 0, integral_approximation_y, color='salmon', alpha=0.3)  # Color the area under the curve
-ax[1].set_ylabel("Integral Approximation")
+# Plot for the integral approximation
+x_range_vals = np.linspace(x_range[0], x_range[1], 1000)
+y_range_vals = f(x_range_vals)
+ax[1].plot(x_range_vals, y_range_vals, label='f(x) = x^2 + 17x + 9', color='b')
+ax[1].fill_between(x_range_vals, y_range_vals, color='skyblue', alpha=0.3)  # Color the area under the curve
+ax[1].axhline(0, color='black',linewidth=0.5)
+ax[1].axvline(x_range[0], color='red',linewidth=0.5, linestyle='--', label='Rentang Integral')
+ax[1].axvline(x_range[1], color='red',linewidth=0.5, linestyle='--')
+ax[1].set_ylabel("f(x)")
 ax[1].set_xlabel("x")
-ax[1].tick_params(axis='y', labelsize=20)
-ax[1].tick_params(axis='x', labelsize=15)
+ax[1].tick_params(axis='y', labelsize=15)
+ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=30, ha='right')
+ax[1].tick_params(axis='x', labelsize=12)
 ax[1].grid(color='green', linestyle='-.', linewidth=.5)
-
-plt.tight_layout()
-st.pyplot(fig)
 
 # Display integral value
 st.write('Nilai integral menggunakan metode trapesium:', integral_value)
+
