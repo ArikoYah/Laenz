f34060c100b038eb4da606fa67a63bc81d4a85ac
Ariko Yahya Setyawan <152178505+ArikoYah@users.noreply.github.com>
2024-02-21 14:46:25 +0700

    Update sliderlaenz.py

--- sliderlaenz.py
+++ sliderlaenz.py
@@ -16,11 +16,14 @@ def trapezoidal_integral(f, a, b, n):
 
 # Streamlit app
 st.title('Integral Trapesium untuk Fungsi f(x) = x^2 + 17x + 9')
-st.write('Gunakan slider di bawah untuk memilih rentang nilai x.')
+st.write('Gunakan slider di bawah untuk memilih rentang nilai x:')
 
 # Slider untuk memilih rentang nilai x
 x_range = st.slider('Rentang nilai x', -20.0, 5.0, (-20.0, 5.0))
 st.write('Rentang nilai x:', x_range)
+
+# Menampilkan nilai rentang integral
+st.write('Rentang integral:', x_range[0], 'hingga', x_range[1])
 
 # Menghitung nilai fungsi untuk rentang yang dipilih
 x_vals = np.linspace(x_range[0], x_range[1], 1000)
@@ -38,6 +41,6 @@ ax.tick_params(axis='x', labelsize=15)
 plt.grid(color='green', linestyle='-.', linewidth=.5)
 st.pyplot(fig)
 
-# Menampilkan integral trapesium dengan dua desimal
+# Menampilkan integral trapesium
 integral_value = trapezoidal_integral(f, x_range[0], x_range[1], 1000)
-st.write(f'Nilai integral menggunakan metode trapesium: {integral_value:.2f}')
+st.write('Nilai integral menggunakan metode trapesium:', integral_value)
