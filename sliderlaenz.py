82ec4c79e3a2c70f7ced0875b76448a3f6e966ea
Ariko Yahya Setyawan <152178505+ArikoYah@users.noreply.github.com>
2024-02-21 14:03:11 +0700

    Update sliderlaenz.py

--- sliderlaenz.py
+++ sliderlaenz.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+from scipy.integrate import quad
 
 # Definisi fungsi
 def f(x):
     return x**2 + 17*x + 9
 
+# Fungsi untuk menghitung integral
+def integral_f(x_min, x_max):
+    return quad(f, x_min, x_max)[0]
+
 # Streamlit app
-st.title('Grafik Fungsi f(x) = x^2 + 17x + 9')
+st.title('Grafik Fungsi f(x) = x^2 + 17x + 9 dan Integralnya')
 st.write('Gunakan slider di bawah untuk memilih rentang nilai x:')
 
 # Slider untuk memilih rentang nilai x
@@ -30,4 +35,9 @@ st.pyplot(plt)
 
 # Menampilkan nilai fungsi untuk setiap nilai x di rentang yang dipilih
 st.write('Nilai fungsi untuk rentang x yang dipilih:')
-st.write(np.column_stack((x_vals, y_vals)))
+data = np.column_stack((x_vals, y_vals))
+st.write(data)
+
+# Menghitung dan menampilkan integral fungsi di rentang yang dipilih
+integral_value = integral_f(x_range[0], x_range[1])
+st.write(f'Nilai integral dari fungsi di rentang yang dipilih: {integral_value}')
